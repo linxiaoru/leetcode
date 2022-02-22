1a2e7aedc4da68da651a5fdb4bda6f1f52139980
cheryl.lin <cheryl.lin@ximalaya.com>
2022-02-22 13:43:44 +0800

    update

---
+++ 栈相关/index.js
@@ -0,0 +1,12 @@
+const stack = [];
+
+// 入栈
+stack.push(1);
+stack.push(2);
+
+// 栈顶元素
+const topItem = stack[stack.length - 1]; // 2
+
+// 出栈
+const item1 = stack.pop(); // 2
+const item2 = stack.pop();
