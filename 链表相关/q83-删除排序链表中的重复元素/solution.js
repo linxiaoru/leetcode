dc26f30201b758fd4a7cae6e7488917efc7c0fe9
cheryl.lin <linxiaoruxr@163.com>
2022-02-25 00:23:19 +0800

    feat: 新增q83

---
+++ 链表相关/q83-删除排序链表中的重复元素/solution.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function (head) {
+  let p = head;
+  while (p && p.next) {
+    // 如果相邻的两个值相等，则快的那个指针后移一位，如果不相等，则慢的指针右移一位
+    if (p.val === p.next.val) {
+      p.next = p.next.next;
+    } else {
+      p = p.next;
+    }
+  }
+  return head;
+};
