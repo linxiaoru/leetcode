d5aa2ae65fb89aff1e911a785a250cc9fa13a74c
cheryl.lin <cheryl.lin@ximalaya.com>
2022-03-03 14:08:36 +0800

    feat: 字典方案解决q1、q20

---
+++ 字典/q20-有效的括号/solution.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+  if (s.length % 2 == 1) {
+    // 奇数直接返回 false，不匹配
+    return false;
+  }
+
+  const strStack = [];
+  const map = new Map();
+  map.set('(', ')');
+  map.set('[', ']');
+  map.set('{', '}');
+
+  for (let i = 0; i < s.length; i++) {
+    let strItem = s[i];
+    let topItem = strStack[strStack.length - 1];
+    if (map.get(strItem)) {
+      strStack.push(strItem);
+    } else if (map.get(topItem) === strItem) {
+      strStack.pop();
+    } else {
+      return false;
+    }
+  }
+  return strStack.length === 0;
+};
